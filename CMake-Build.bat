2e9e040339e984abeefc30998e0483fd5c8cfc15
YawDev <nik.xeiladakis@gmail.com>
2024-07-26 13:52:43 +0300

    beautify

--- CMake-Build.bat
+++ CMake-Build.bat
@@ -1,8 +1,13 @@
+@echo off
 
-mkdir build
-cd build
-cmake ..
-cmake --build .
-cd ..
-move "build\Debug\SnapKey.exe" "."
-rmdir /s /q build
+mkdir build > nul
+cd build > nul
+echo [+] Preparing files...
+cmake .. > nul
+echo [+] Compiling...
+cmake --build . > nul
+cd .. > nul
+move "build\Debug\SnapKey.exe" "." > nul
+echo [+] Done!
+rmdir /s /q build > nul
+pause > nul
